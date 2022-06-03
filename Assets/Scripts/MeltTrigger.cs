73b0fa844d5bda587e64c2f355d510ce82a5bcd7
Patrick Gudikunst <pgudikun@uci.edu>
2022-06-02 22:44:51 -0700

    STEAM

--- Assets/Scripts/MeltTrigger.cs
+++ Assets/Scripts/MeltTrigger.cs
@@ -7,6 +7,10 @@ public class MeltTrigger : MonoBehaviour
     #region Public Vars
 
     public GameObject myCube; // Can be assigned without Start() but requires inspector
+
+    public GameObject mySteam;
+    private ParticleSystem steamParticles;
+
     public Vector3 sizeChange; // Modify this in the Inspector
     // [HideInInspector]
     public ObjBaseTemp baseTemp;
@@ -22,6 +26,8 @@ public class MeltTrigger : MonoBehaviour
     private void Start()
     {
         myCube = this.gameObject; // Assigns the gameobject
+        mySteam.SetActive(false);
+        steamParticles = mySteam.GetComponent<ParticleSystem>();
 
         // Adding the temperature condition requires the playerTemp to be obtained here as a getComponent.
         // E.g. float playerTemp = GameObject.FindGameObjectWithTag("Player").GetComponent<Temperature>().GetTemp;
@@ -31,8 +37,14 @@ public class MeltTrigger : MonoBehaviour
         // Behavior: Regardless of tag, collision into a hotter object will melt the current object.
         if ((collision.tag == "Player" || collision.tag == "CanHasTemp") && baseTemp.GetBaseTemp() < collision.GetComponent<Temperature>().GetTemp())
         {
+            mySteam.SetActive(true);
+            var steamShape = steamParticles.shape;
+            var steamMain = steamParticles.main;
+
             // Subtracts the scale Vector3 when the object is touched.
-            myCube.transform.localScale = myCube.transform.localScale - sizeChange;
+            //myCube.transform.localScale = myCube.transform.localScale - sizeChange;
+            myCube.transform.localScale -= sizeChange;
+            steamShape.radius = steamShape.radius - (sizeChange.x / 2);
 
             // Since you cannot compare vectors using ==, you must calculate the distance instead.
             float tempDist = Vector3.Distance(myCube.transform.localScale, Vector3.zero);
@@ -41,9 +53,21 @@ public class MeltTrigger : MonoBehaviour
             if (tempDist <= 0.5f)
             {
                 Destroy(myCube);
+                steamMain.loop = false;
             }
         }
     }
+
+    /*
+    private void OnTriggerExit(Collider collision)
+    {
+        if ((collision.tag == "Player" || collision.tag == "CanHasTemp") && baseTemp.GetBaseTemp() < collision.GetComponent<Temperature>().GetTemp())
+        {
+            Invoke("Steam lasts for a little bit", 5.0f);
+            mySteam.SetActive(false);
+        }
+    }
+    */
 
     #endregion
 }
